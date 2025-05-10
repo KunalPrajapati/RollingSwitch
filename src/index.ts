f8547c8834e49e9adda0226708b367f4d56a90d1
Kunal Prajapati <kunalprajpati@gmail.com>
2025-05-10 11:45:34 +0530

    minor bug fixed

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import RollingSwitch from '../src/components/RollingSwitch';
-import RollingMoonSwitch from '../src/components/RollingMoonSwitch';
-
-export { RollingSwitch, RollingMoonSwitch };
-export default RollingSwitch;
